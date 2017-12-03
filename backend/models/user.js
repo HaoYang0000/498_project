71665ca74b383862a81573e180ee2e40bc22f7df
deltadu <dujianlin@berkeley.edu>
2017-12-02 21:19:43 -0600

    updated user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,16 +2,26 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
 var userSchema = mongoose.Schema({
-    email		: String,
-    password	: String
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    user_name: {type: String, unique: true},
+    age: Number,
+    city: String,
+    state: String,
+    country: String,
+    pet_spiecie: String,
+    prefered_species: String,
+    gender: String,
+    prefered_age_min: Number,
+    prefered_age_max: Number
 });
 
 userSchema.methods.generateHash = function(password) {
-	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+ return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 userSchema.methods.validPassword = function(password) {
-	return bcrypt.compareSync(password, this.password);
+ return bcrypt.compareSync(password, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
