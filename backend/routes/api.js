316605fa48277a79a97168f106b19aaa1c017cdc
deltadu <dujianlin@berkeley.edu>
2017-12-07 19:24:40 -0600

    added more detail to user schema & populate queue to api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -63,14 +63,14 @@ module.exports = function(router, passport) {
             }
         });
     });
-    
+
     router.get('/get_current_user',
         function(req, res) {
             console.log(req.isAuthenticated());
             res.status(200).json({ user: req.user
         });
     });
-    
+
 
     //----------------Show the User Table--------------------------
     router.get('/users', function(req, res){
@@ -108,11 +108,54 @@ module.exports = function(router, passport) {
     router.put('/story', function(req, res){
         //TODO
     });
+    
+    //----------------Populate queue-------------------jianlin--------
+    router.put('/populateQueue/:id', function(req, res){
+        //TODO
+        //get user id
+        var user_id = req.params.id;
 
-    //----------------Like User--------------------------------
+        //get user preference filter
+        var user_preference;
+        User.find(req.params.id, function(err, user) {
+            user_preference = user;
+        });
+
+        User.find("prefered_species=\'" + user_preference.pre + "\'").limit(100);
+
+    })
+
+
+
+//----------------Like User--------------------------------
     router.put('/like', function(req, res){
         //TODO
-    });
+        var userPost = {
+            name: req.body.user_id,
+        }
+
+
+        User.findByIdAndUpdate(req.params.id, userPost, {new: true}, function(err, user) {
+        if(err) {
+            res.status(500).send({
+                message: err,
+                data: []
+            });
+        } else {
+            if (user) {
+                res.status(200).send({
+                    message: 'OK',
+                    data: user
+                });
+            } else {
+                res.status(404).send({
+                    message: 'User does not exist',
+                    data: []
+                });
+            }
+
+        });
+    }
 
     //----------------Delete User------------------------------
     router.delete('/user', function(req, res){
